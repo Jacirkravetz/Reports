d0742692299a6a2134eebe385d57a39b2adf3947
Jacir Kravetz <jacirkravetz@gmail.com>
2024-02-15 08:22:06 -0300

    Tests

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,162 +1,164 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.ServiceModel;
-//using System.Threading.Tasks;
-//using ServiceReference1;
+﻿using System;
+using System.Reflection;
+using System.ServiceModel;
+using System.Threading.Tasks;
+using ConsoleTables;
+using ServiceReference1;
 
-//namespace RSWcf
-//{
-//    class Program
-//    {
-//        static ReportingService2010SoapClient rsclient = null;
+class Program
+{
+    static ReportingService2010SoapClient rsclient = null;
 
-//        public static void Main(string[] args)
-//        {
-//            BasicHttpBinding rsBinding = new BasicHttpBinding();
-//            rsBinding.Security.Mode = BasicHttpSecurityMode.TransportCredentialOnly;
-//            rsBinding.Security.Transport.ClientCredentialType = HttpClientCredentialType.Ntlm;
+    static async System.Threading.Tasks.Task Main(string[] args)
+    {
+        InitializeReportingServiceClient();
 
-//            EndpointAddress rsEndpointAddress = new EndpointAddress("http://desktop-hu97nsf/reportserver/ReportService2010.asmx");
+        // Define o caminho da pasta onde os DataSources estão localizados
+        string folderPath = "/DataSources";
 
-//            rsclient = new ReportingService2010SoapClient(rsBinding, rsEndpointAddress);
+        // Exemplo: Obter conteúdo de um DataSource
+        string dataSourcePath = $"{folderPath}/FonteTeste";
+        await GetAndPrintDataSourceContent(dataSourcePath);
 
-//            var output = rsListChildren("/");
-//            output.Wait();
+        // Testar conexão do DataSource
+        bool testResult = await TestDataSourceConnection(dataSourcePath, "admin", "senha@123");
+        Console.WriteLine($"Resultado do teste de conexão para {dataSourcePath}: {testResult}");
 
-//            if (output.Status == TaskStatus.RanToCompletion && output.Result.Length > 0)
-//            {
-//                foreach (CatalogItem item in output.Result)
-//                {
-//                    Console.WriteLine($"Item Path: {item.Path}");
-//                }
-//            }
+        // Atualizar senha do DataSource
+        await UpdateDataSourcePasswordAsync(dataSourcePath, "senha@123");
 
-//            // var testConnectOutput = await TestDataSourceConnection("/DataSources/FonteTeste ", "admin", "senha@UBS");
+        Console.WriteLine("Pressione qualquer tecla para sair.");
+        Console.ReadKey();
+    }
 
-//            // await UpdateDataSourcePasswordAsync("/DataSources/FonteTeste ", "senha@UBS");
+    private static void InitializeReportingServiceClient()
+    {
+        BasicHttpBinding rsBinding = new BasicHttpBinding();
+        rsBinding.Security.Mode = BasicHttpSecurityMode.TransportCredentialOnly;
+        rsBinding.Security.Transport.ClientCredentialType = HttpClientCredentialType.Ntlm;
+
+        EndpointAddress rsEndpointAddress = new EndpointAddress("http://desktop-hu97nsf/reportserver/ReportService2010.asmx");
+
+        rsclient = new ReportingService2010SoapClient(rsBinding, rsEndpointAddress);
+    }
+
+    private static async System.Threading.Tasks.Task GetAndPrintDataSourceContent(string dataSourcePath)
+    {
+        TrustedUserHeader trustedUserHeader = new TrustedUserHeader();
+
+        try
+        {
+            // Obter conteúdo do DataSource
+            var getDataSourceResponse = await rsclient.GetDataSourceContentsAsync(trustedUserHeader, dataSourcePath);
+            DataSourceDefinition dataSourceDefinition = getDataSourceResponse.Definition;
+
+            // Imprimir detalhes do DataSource em formato de tabela
+            Console.WriteLine($"DataSource Content for: {dataSourcePath}");
+            //PrintTable("Property", "Value", dataSourceDefinition.ConnectString, dataSourceDefinition.UserName, dataSourceDefinition.Password);
+            PrintTable(dataSourceDefinition);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error getting DataSource content: {ex.Message}");
+        }
+    }
+
+    private static async Task<bool> TestDataSourceConnection(string dataSourcePath, string userName, string password)
+    {
+        TrustedUserHeader trustedUserHeader = new TrustedUserHeader();
+
+        try
+        {
+            // Obter conteúdo do DataSource
+            var getDataSourceResponse = await rsclient.GetDataSourceContentsAsync(trustedUserHeader, dataSourcePath);
+            DataSourceDefinition dataSourceDefinition = getDataSourceResponse.Definition;
+            dataSourceDefinition.Password = password;
+
+            // Testar conexão do DataSource
+            var testConnectResponse = await rsclient.TestConnectForDataSourceDefinitionAsync(
+                new TestConnectForDataSourceDefinitionRequest
+                {
+                    TrustedUserHeader = trustedUserHeader,
+                    UserName = userName,
+                    Password = password,
+                    DataSourceDefinition = dataSourceDefinition
+                });
+
+            // Imprimir resultados do teste em formato de tabela
+            Console.WriteLine($"Connection Test Result for: {dataSourcePath}");
+            PrintTable(testConnectResponse);
+
+            return testConnectResponse.TestConnectForDataSourceDefinitionResult;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error testing DataSource connection: {ex.Message}");
+            return false;
+        }
+    }
+
+    private static async System.Threading.Tasks.Task UpdateDataSourcePasswordAsync(string dataSourcePath, string newPassword)
+    {
+        TrustedUserHeader trustedUserHeader = new TrustedUserHeader();
+
+        try
+        {
+            // Obter conteúdo do DataSource
+            var getDataSourceResponse = await rsclient.GetDataSourceContentsAsync(trustedUserHeader, dataSourcePath);
+            DataSourceDefinition dataSourceDefinition = getDataSourceResponse.Definition;
+
+            // Atualizar a senha no DataSourceDefinition
+            dataSourceDefinition.Password = newPassword;
+
+            // Definir o conteúdo atualizado do DataSource
+            var setDataSourceResponse = await rsclient.SetDataSourceContentsAsync(trustedUserHeader, dataSourcePath, dataSourceDefinition);
+
+            Console.WriteLine($"DataSource password updated successfully for {dataSourcePath}.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error updating DataSource password: {ex.Message}");
+        }
+    }
+
+    private static void PrintTable(object dataObject)
+    {
+        // Obter todas as propriedades públicas do objeto
+        PropertyInfo[] properties = dataObject.GetType().GetProperties();
+
+        // Definir o número máximo de propriedades por linha
+        const int propertiesPerRow = 13;
+
+        // Dividir as propriedades em grupos
+        var propertyGroups = properties.Select((p, index) => new { Index = index, Property = p })
+                                       .GroupBy(x => x.Index / propertiesPerRow, x => x.Property)
+                                       .ToList();
+
+        // Criar a tabela usando o ConsoleTables
+        var table = new ConsoleTable(properties.Select(p => p.Name).ToArray());
+
+        // Adicionar linhas à tabela com os valores das propriedades
+        foreach (var propertyGroup in propertyGroups)
+        {
+            var rowData = new object[propertiesPerRow];
+
+            for (int i = 0; i < propertiesPerRow; i++)
+            {
+                var propertyIndex = propertyGroup.ElementAtOrDefault(i);
+
+                if (propertyIndex != null)
+                {
+                    rowData[i] = propertyIndex.GetValue(dataObject);
+                }
+            }
+            table.AddRow(rowData);
+        }
+
+        // Imprimir a tabela
+        Console.WriteLine(table.ToString());
+    }
 
 
-//            //var dataSourceOutput = ListDataSources("/");
-//            //dataSourceOutput.Wait();
-
-//            //if (dataSourceOutput.Status == TaskStatus.RanToCompletion && dataSourceOutput.Result.Length > 0)
-//            //{
-//            //    foreach (DataSourcePrompt dataSource in dataSourceOutput.Result)
-//            //    {
-//            //        Console.WriteLine($"Data Source Name: {dataSource.Name}");
-
-//            //        //// Testing DataSource connection before updating password
-//            //        //var testConnectOutput = TestDataSourceConnection(dataSource.Path, "TestUserName", "TestPassword");
-//            //        //testConnectOutput.Wait();
-
-//            //        //if (testConnectOutput.Status == TaskStatus.RanToCompletion && testConnectOutput.Result)
-//            //        //{
-//            //        //    Console.WriteLine("DataSource connection test passed. Proceeding with password update.");
-
-//            //        //    // Update DataSource password
-//            //        //    var updateDataSourceOutput = UpdateDataSourcePasswordAsync(dataSource.Path, "NewPassword");
-//            //        //    updateDataSourceOutput.Wait();
-//            //        //}
-//            //        //else
-//            //        //{
-//            //        //    Console.WriteLine("DataSource connection test failed. Skipping password update.");
-//            //        //}
-//            //    }
-//            //}
-
-//            //TrustedUserHeader trustedUserHeader = new TrustedUserHeader();
-//            Console.WriteLine("Completed!");
-//            Console.ReadLine();
-//        }
-
-//        private static async Task<CatalogItem[]> rsListChildren(string itemPath)
-//        {
-//            TrustedUserHeader trustedUserHeader = new TrustedUserHeader();
-//            ListChildrenResponse listChildrenResponse = null;
-
-//            try
-//            {
-//                listChildrenResponse = await rsclient.ListChildrenAsync(trustedUserHeader, itemPath, false);
-//            }
-//            catch (Exception exception)
-//            {
-//                Console.WriteLine($"Error listing children: {exception.Message}");
-//                return new CatalogItem[0];
-//            }
-
-//            return listChildrenResponse.CatalogItems;
-//        }
-
-//        private static async Task<DataSourcePrompt[]> ListDataSources(string itemPath)
-//        {
-//            TrustedUserHeader trustedUserHeader = new TrustedUserHeader();
-//            // ListDataSourcesResponse listDataSourcesResponse = null;
-//            GetItemDataSourcePromptsResponse listDataSourcesResponse;
-//            try
-//            {
-//                listDataSourcesResponse = await rsclient.GetItemDataSourcePromptsAsync(trustedUserHeader, itemPath);
-//            }
-//            catch (Exception exception)
-//            {
-//                Console.WriteLine($"Error listing data sources: {exception.Message}");
-//                return new DataSourcePrompt[0];
-//            }
-
-//            return listDataSourcesResponse.DataSourcePrompts;
-//        }
-
-//        private static async Task<bool> TestDataSourceConnection(string dataSourcePath, string userName, string password)
-//        {
-//            TrustedUserHeader trustedUserHeader = new TrustedUserHeader();
-
-//            // Get DataSource contents
-//            var getDataSourceResponse = await rsclient.GetDataSourceContentsAsync(trustedUserHeader, dataSourcePath);
-//            DataSourceDefinition dataSourceDefinition = getDataSourceResponse.Definition;
 
 
-
-//            TestConnectForDataSourceDefinitionRequest connectForDataSourceDefinitionRequest = new TestConnectForDataSourceDefinitionRequest();
-//            connectForDataSourceDefinitionRequest.TrustedUserHeader = trustedUserHeader;
-//            connectForDataSourceDefinitionRequest.UserName = userName;
-//            connectForDataSourceDefinitionRequest.Password = password;
-//            connectForDataSourceDefinitionRequest.DataSourceDefinition = dataSourceDefinition;
-
-//            try
-//            {
-//                // Test the connection for the given DataSource
-//                var testConnectResponse = await rsclient.TestConnectForDataSourceDefinitionAsync(connectForDataSourceDefinitionRequest);
-
-
-//                return testConnectResponse.TestConnectForDataSourceDefinitionResult;
-//            }
-//            catch (Exception ex)
-//            {
-//                Console.WriteLine($"Error testing DataSource connection: {ex.Message}");
-//                return false;
-//            }
-//        }
-
-//        private static async System.Threading.Tasks.Task UpdateDataSourcePasswordAsync(string dataSourcePath, string newPassword)
-//        {
-//            TrustedUserHeader trustedUserHeader = new TrustedUserHeader();
-//            try
-//            {
-//                // Get DataSource contents
-//                var getDataSourceResponse = await rsclient.GetDataSourceContentsAsync(trustedUserHeader, dataSourcePath);
-//                DataSourceDefinition dataSourceDefinition = getDataSourceResponse.Definition;
-
-//                // Update the password in the DataSourceDefinition
-//                dataSourceDefinition.Password = newPassword;
-
-//                // Set updated DataSource contents
-//                var setDataSourceResponse = await rsclient.SetDataSourceContentsAsync(trustedUserHeader, dataSourcePath, dataSourceDefinition);
-
-//                Console.WriteLine($"DataSource password updated successfully for {dataSourcePath}.");
-//            }
-//            catch (Exception ex)
-//            {
-//                Console.WriteLine($"Error updating DataSource password: {ex.Message}");
-//            }
-//        }
-//    }
-//}
+}
